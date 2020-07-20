59f0e974d8e152e2943ddac51c2dffdda622ac6e
SatyapragyanDas <spdasbjem@gmail.com>
2020-07-21 02:06:10 +0530

    Second Commit

--- app/src/main/java/com/example/tictactoygame/MainActivity.java
+++ app/src/main/java/com/example/tictactoygame/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,7 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.refreshmenu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.Refresh:
+                Toast.makeText(this,"Refresh",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.Settings:
+                Toast.makeText(this,"Settings",Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
     public void buClick(View view) {
         Button buSelected=(Button) view;
         int cellId=0;
